d69fd6cc1d3589f4fe73cc434c6da1b5ddda2f9b
Fernando Longhi de Andrade <fernandolonghideandrade@gmail.com>
2025-05-27 23:32:35 -0300

    feat: add programs, commands and summary

---
+++ P2/programs/binary_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 2000000
+
+int binary_search(int arr[], int size, int key) {
+    int left = 0, right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == key) return mid;
+        if (arr[mid] < key) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int *arr = (int*)malloc(N * sizeof(int));
+    for (int i = 0; i < N; i++) {
+        arr[i] = i;  // vetor ordenado para busca
+    }
+
+    int key = N-1;
+    int result = binary_search(arr, N, key);
+    printf("Done Binary Search. Found at index: %d\n", result);
+
+    free(arr);
+    return 0;
+}
